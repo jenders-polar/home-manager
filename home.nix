61ef630a941c8b2aa7248e93a3247a45ad1e78ad
Jacob Enders <jacobenders1@gmail.com>
2026-07-17 08:30:42 -0400

    cleanup linebreaks

--- home.nix
+++ home.nix
@@ -1,33 +1,33 @@
 { pkgs, ... }: let
-  nixvim = import (fetchGit {
-    url = "https://github.com/nix-community/nixvim";
-  });
+	nixvim = import (fetchGit {
+		url = "https://github.com/nix-community/nixvim";
+	});
 in {
-  imports = [
-    nixvim.homeModules.nixvim
+	imports = [
+		nixvim.homeModules.nixvim
 		./programs/nixvim.nix
 		./programs/direnv.nix
 		./programs/lazydocker.nix
 		./programs/lazygit.nix
 		./programs/btop.nix
 		./programs/fish.nix
-  ];
+	];
 
-  home.username = "jenders";
-  home.homeDirectory = "/home/jenders";
-  home.stateVersion = "23.11";
+	home.username = "jenders";
+	home.homeDirectory = "/home/jenders";
+	home.stateVersion = "23.11";
 	home.shell.enableFishIntegration = true;
 
-  home.packages = [
+	home.packages = [
 		pkgs.devpod
 		pkgs.devenv
 	];
 
-  home.file = {};
+	home.file = {};
 
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
+	home.sessionVariables = {
+		EDITOR = "nvim";
+	};
 
-  programs.home-manager.enable = true;
+	programs.home-manager.enable = true;
 }
