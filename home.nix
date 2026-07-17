befdd77c3439a73088fa0fe2b6efc79bf947090d
Jacob Enders <jacobenders1@gmail.com>
2026-07-17 08:28:00 -0400

    update

--- home.nix
+++ home.nix
@@ -10,11 +10,13 @@ in {
 		./programs/lazydocker.nix
 		./programs/lazygit.nix
 		./programs/btop.nix
+		./programs/fish.nix
   ];
 
   home.username = "jenders";
   home.homeDirectory = "/home/jenders";
   home.stateVersion = "23.11";
+	home.shell.enableFishIntegration = true;
 
   home.packages = [
 		pkgs.devpod
